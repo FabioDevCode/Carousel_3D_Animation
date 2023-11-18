991c2be82fd7080023a3ed9acc4b7f435f1594ef
FabioDevCode <0677384849@orange.fr>
2023-11-18 22:32:40 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+const box = document.querySelector('.box');
+const card = document.querySelectorAll('.card');
+
+let degrees = 0;
+
+prev.addEventListener('click', function() {
+    degrees += 45;
+    box.style = `transform: perspective(1000px) rotateZ(0deg) rotateX(0deg) rotateY(${degrees}deg);`;
+})
+
+next.addEventListener('click', function() {
+    degrees -= 45;
+    box.style = `transform: perspective(1000px) rotateZ(0deg) rotateX(0deg) rotateY(${degrees}deg);`;
+});
+
+
+window.addEventListener('scroll', function(e) {
+    const windowTop = this.document.documentElement.scrollTop;
+
+    // if(windowTop < 1000) {
+    //     const roX = xdeg / 100 * timeLine;
+    //     const per = (persv - 1000) / 100 * timeLine + 1000;
+    //     const cZ = 50 + (350 - (350 / 100 * timeLine));
+
+    //     box.style = `transform: rotateX(-${roX}deg) perspective(${per}px) rotateZ(0deg) rotateY(0deg);`;
+    //     for(let j = 0; j < card.length; j++) {
+    //         card[j].style = `transform: rotateY(calc(${j} * 45deg)) translateZ(${cZ}px);`;
+    //     };
+    // }
+
+    if(windowTop < 1000) {
+        for(let j = 0; j < card.length; j++) {
+            card[j].style = `transform: rotateY(calc(${j} * 45deg)) translateZ(50px);`;
+        };
+        box.style = `transform: rotateX(-90deg) perspective(15000px) rotateZ(0deg) rotateY(0deg);`;
+    }
+    if(windowTop > 1000) {
+        for(let j = 0; j < card.length; j++) {
+            card[j].style = `transform: rotateY(calc(${j} * 45deg)) translateZ(400px);`;
+        };
+        box.style = `transform: rotateX(0deg) perspective(1000px) rotateZ(0deg) rotateY(0deg);`;
+    }
+})
